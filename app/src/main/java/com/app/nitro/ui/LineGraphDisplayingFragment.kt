5a259935721c3f52df42b6b42115852194739105
Praveen Manepalli <manepally.praveen@gmail.com>
2023-02-21 16:06:40 +0530

    Line and Bar Graph display using MpAndroidhart1

--- app/src/main/java/com/app/nitro/ui/LineGraphDisplayingFragment.kt
+++ app/src/main/java/com/app/nitro/ui/LineGraphDisplayingFragment.kt
@@ -54,9 +54,8 @@ class LineGraphDisplayingFragment : Fragment() {
 
 
 
-       //TODO    you have to use your api cal  to get the response
-        //TODO Response Should lokk like 15:52:35.331  I
-        // result: GraphDataModel(data=GraphData(day=
+       //TODO    you have to use your api cal  to get the response   Response Should lokk like
+        // GraphDataModel(data=GraphData(day=
         // [Day(hour=0.0, value=3.0666666666666664), Day(hour=1.0, value=3.0675), Day(hour=2.0, value=3.068), Day(hour=3.0, value=3.0683333333333334), Day(hour=4.0, value=3.068), Day(hour=5.0, value=3.067), Day(hour=6.0, value=3.067), Day(hour=7.0, value=3.068), Day(hour=8.0, value=3.0686666666666667), Day(hour=9.0, value=3.0660000000000003), Day(hour=10.0, value=3.067)],
         // month=[Month(day=2023-02-01, value=3.0009565217391305), Month(day=2023-02-02, value=3.0673432835820895), Month(day=2023-02-03, value=3.067711538461538), Month(day=2023-02-04, value=3.0679166666666666), Month(day=2023-02-06, value=3.069529411764706), Month(day=2023-02-07, value=3.069551724137931), Month(day=2023-02-08, value=3.0695172413793106), Month(day=2023-02-09, value=3.069641791044776), Month(day=2023-02-10, value=3.068622222222222), Month(day=2023-02-14, value=3.0681875), Month(day=2023-02-15, value=3.068169230769231), Month(day=2023-02-16, value=3.068234375), Month(day=2023-02-17, value=3.067863636363636), Month(day=2023-02-18, value=3.0679830508474577), Month(day=2023-02-19, value=3.068076923076923), Month(day=2023-02-20, value=3.068108695652174), Month(day=2023-02-21, value=3.0675)], week=[Week(day=2023-02-19, value=3.068076923076923),
         // Week(day=2023-02-20, value=3.068108695652174), Week(day=2023-02-21, value=3.0675)], year=[Year(month=1, value=3.1369675190048376), Year(month=2, value=3.064716666666667)]), status=true, message=null)
